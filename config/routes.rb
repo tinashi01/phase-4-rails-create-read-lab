72d5587febb88c1b39ad12c2cf15944eb54325c1
Tina Shi <init.defaultBranch>
2023-06-16 14:21:49 +1000

    commit tree 440569bdbf672d2a1c883d5a3d879a6603f4336c

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/plants', to: 'plants#index'
+  get '/plants/:id', to: 'plants#show'
+  post '/plants', to: 'plants#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
